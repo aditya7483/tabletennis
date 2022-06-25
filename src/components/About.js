fbc3a73557f2f134c56997a41d1f9f15ad42b0ff
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-25 13:16:33 +0530

    responsive changes

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className='my-3 mb-5' id='about'>
       <h1 className='mb-5 myhead-3 head-font text-center fw-bold'>ABOUT</h1>
-      <div id='about-box' className='container d-flex flex-row p-0 mb-5'>
+      <div id='about-box' className='container d-flex flex-lg-row p-0 mb-5 border'>
         <img src={Coach} alt='..' className='coach-pic' />
         <div className='py-2 px-1 d-flex flex-column align-items-center'>
           <h2 className='text-center my-2'>DANIEL NARODITSKY</h2>
@@ -17,36 +17,49 @@ export default function About() {
             <i className="fa-brands fa-instagram social"></i>
           </div>
         </div>
-      </div>
-      <div id="dummy-div-1">
+        {/* 
+        <div id="dummy-div-1">
+
+        </div>
+        <div id="dummy-div-2">
+
+        </div> */}
 
       </div>
-      <div id="dummy-div-2">
-
-      </div>
-
       <hr className=' myline'></hr>
 
       <h1 className='my-5 myhead-3 head-font text-center fw-bold'>ACHIEVEMENTS</h1>
 
-      <div className='d-flex justify-content-md-around align-items-center'>
-        <div className="card mycard-1" style={{ width: "18rem" }}>
-          <div className="card-body">
-            <i className=" card-title fa-solid fa-trophy social2"></i>
-            <h3 className='my-4 text-center myhead-4'>3 TIMES STATE CHAMPION</h3>
+      <div className='d-flex container-fluid justify-content-lg-around align-items-center'>
+        <div className='row'>
+
+          <div className=' col-lg-4 my-4'>
+            <div className="card mycard-1" style={{ width: "18rem" }}>
+              <div className="card-body">
+                <i className=" card-title fa-solid fa-trophy social2"></i>
+                <h3 className='my-4 text-center myhead-4'>3 TIMES STATE CHAMPION</h3>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="card mycard-1" style={{ width: "18rem" }}>
-          <div className="card-body">
-            <i className=" card-title fa-solid fa-table-tennis-paddle-ball social2"></i>
-            <h3 className='my-4 text-center myhead-4'>COACHED 3 NATIONAL LEVEL PLAYERS</h3>
+
+          <div className=' col-lg-4 my-4'>
+            <div className="card mycard-1" style={{ width: "18rem" }}>
+              <div className="card-body">
+                <i className=" card-title fa-solid fa-table-tennis-paddle-ball social2"></i>
+                <h3 className='my-4 text-center myhead-4'>COACHED 3 NATIONAL LEVEL PLAYERS</h3>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="card mycard-1" style={{ width: "18rem" }}>
-          <div className="card-body">
-            <i className=" card-title fa-solid fa-medal social2"></i>
-            <h3 className='my-4 text-center myhead-4'>RECIEVED BEST PLAYER OF THE YEAR AWARD</h3>
+
+          <div className=' col-lg-4 my-4'>
+            <div className="card mycard-1" style={{ width: "18rem" }}>
+              <div className="card-body">
+                <i className=" card-title fa-solid fa-medal social2"></i>
+                <h3 className='my-4 text-center myhead-4'>RECIEVED BEST PLAYER OF THE YEAR AWARD</h3>
+              </div>
+            </div>
           </div>
+
         </div>
       </div>
     </div>
